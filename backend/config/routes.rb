89d2dc343d3e48d10510fb451d2989530cab1279
imrie123 <nosirie@ymobile.ne.jp>
2024-07-09 01:25:59 +0900

    Add user login functionality using Firebase by modifying UsersController, adding FirebaseService (sign_in.rb), and updating routes.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :users, only: [:create] do
+    collection do
+      post :sign_in
+    end
   end
 
 end
